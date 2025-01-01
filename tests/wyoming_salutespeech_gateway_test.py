e869f55205868b4449127a4684edb7305fc4efe4
sergemart <serge.martynov@gmail.com>
2025-01-01 22:49:26 +0300

    Remove obsolete test.

--- tests/wyoming_salutespeech_gateway_test.py
+++
@@ -1,4 +0,0 @@
-from wyoming_salutespeech_gateway import salutespeech_adapter
-
-with open('samples/sample2.mp3', 'rb') as audio_file:
-    print ( salutespeech_adapter.recognize( audio=audio_file.read() ) )
